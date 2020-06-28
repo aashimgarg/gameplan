1330365aa3c657491114f21135edc8a6f2f7dde7
aashimgarg <18bcs001@smvdu.ac.in>
2020-06-28 14:29:32 +0530

    added the dummy data for the projects and set up the specific routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
+import React , { Component } from 'react';
+import { BrowserRouter, Switch, Route  } from 'react-router-dom';
 import Navbar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
 
-function App() {
-  return (
-    <BrowserRouter>
-       <div className ="App">
-         <Navbar />
-       </div>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route path='/'component={Dashboard} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
