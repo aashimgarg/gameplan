87ade3b2ea2ebdc030181889c71f632edfa57c25
aashimgarg <18bcs001@smvdu.ac.in>
2020-07-18 15:19:15 +0530

    Updates

---
+++ precache-manifest.3027870a389d5d327ba5b3f6aa0ceefa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b4ea8686544fa8886ec70514cdd6bf",
+    "url": "/firebase-gameplan/index.html"
+  },
+  {
+    "revision": "1a8949a53ed3c2b6febf",
+    "url": "/firebase-gameplan/static/css/main.cc8c5bb0.chunk.css"
+  },
+  {
+    "revision": "fbda7a0e2f3752c8519b",
+    "url": "/firebase-gameplan/static/js/2.6186d2f9.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/firebase-gameplan/static/js/2.6186d2f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a8949a53ed3c2b6febf",
+    "url": "/firebase-gameplan/static/js/main.f373ee1a.chunk.js"
+  },
+  {
+    "revision": "9ca9cc02da8c258c1fce",
+    "url": "/firebase-gameplan/static/js/runtime-main.57167142.js"
+  }
+]);
